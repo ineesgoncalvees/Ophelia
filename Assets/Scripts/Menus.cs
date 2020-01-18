633d842c478bb502f5e66547a4df14116835caae
FiammaV <ines.perdigao.nunes@gmail.com>
2020-01-18 02:34:27 +0000

    Finish Song Select Menu

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -9,12 +9,27 @@ public class Menus : MonoBehaviour
     public GameObject start;
     public GameObject main;
     public GameObject story;
+    public GameObject songSelect;
+
+    public GameObject song1;
+    public GameObject song2;
+
+    public GameObject coverOne;
+    public GameObject coverTwo;
 
     private void Awake()
     {
         start = GameObject.Find("Start");
         main = GameObject.Find("Main");
         story = GameObject.Find("Story");
+        songSelect = GameObject.Find("Song Select");
+
+        song1 = GameObject.Find("Song1");
+        song2 = GameObject.Find("Song2");
+
+        coverOne = GameObject.Find("SongOneCover");
+        coverTwo = GameObject.Find("SongTwoCover");
+
     }
 
     private void Start()
@@ -22,6 +37,13 @@ public class Menus : MonoBehaviour
         start.gameObject.SetActive(true);
         main.gameObject.SetActive(false);
         story.gameObject.SetActive(false);
+        songSelect.gameObject.SetActive(false);
+
+        song1.gameObject.SetActive(false);
+        song2.gameObject.SetActive(false);
+
+        coverOne.gameObject.SetActive(false);
+        coverTwo.gameObject.SetActive(false);
 
         StartCoroutine(StartScreen());
     }
@@ -36,7 +58,14 @@ public class Menus : MonoBehaviour
 
     public void SinglePlayer()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        main.gameObject.SetActive(false);
+        songSelect.gameObject.SetActive(true);   
+        
+        song1.gameObject.SetActive(true);
+        coverOne.gameObject.SetActive(false);
+
+        song2.gameObject.SetActive(true);
+        coverTwo.gameObject.SetActive(false);
     }
 
     public void Multiplayer()
@@ -44,15 +73,46 @@ public class Menus : MonoBehaviour
         //To be implemented
     }
 
+    public void SongOne() {
+        coverOne.gameObject.SetActive(true);
+        coverTwo.gameObject.SetActive(false);
+    }
+
+    public void SongTwo()
+    {
+        coverOne.gameObject.SetActive(false);
+        coverTwo.gameObject.SetActive(true);
+    }
+
     public void Story()
     {
         main.SetActive(false);
         story.SetActive(true);
     }
+
+    public void StartSongOne()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartSongTwo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+
     public void Return()
     {
         story.SetActive(false);
+
+        songSelect.gameObject.SetActive(false);
+
+        song1.gameObject.SetActive(false);
+        song2.gameObject.SetActive(false);
+
+        coverOne.gameObject.SetActive(false);
+        coverTwo.gameObject.SetActive(false);
+
         main.SetActive(true);
-        
     }
 }
